19da89a4bd9581e154096ab794565ae3bd6690cd
Serhat Gün <36711858+serhatgun@users.noreply.github.com>
2023-07-17 13:59:07 +0300

    Function name changes

--- utils.h
+++ utils.h
@@ -16,7 +16,7 @@ private:
     vector<int> out;
 
 public:
-    void my_histogram_equalization(const Mat& im, Mat& finalResult, vector<double>& histogram);
-    Mat my_imgthresholding(const Mat& im, double T = -1);
-    void preprocessing(const Mat& im, Mat& resized_im, Mat& gray_im, Mat& eq_im, Mat& filtered_im, Mat& bin_im, bool show);
+    void histogramEqualization(const Mat& im, Mat& finalResult, vector<double>& histogram);
+    Mat imgThresholding(const Mat& im, double T = -1);
+    void preProcessing(const Mat& im, Mat& resized_im, Mat& gray_im, Mat& eq_im, Mat& filtered_im, Mat& bin_im, bool show);
 };
