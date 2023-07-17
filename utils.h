d6f8ae2d0a8035c8c3b650650f92d806ad6e66d7
Serhat Gün <36711858+serhatgun@users.noreply.github.com>
2023-07-17 14:13:29 +0300

    Plate region and character extraction functions added.

--- utils.h
+++ utils.h
@@ -15,8 +15,12 @@ private:
     vector<double> cumulative;
     vector<int> out;
 
+    void adaptiveCalc(const Mat& im, int row, int col, const Mat& mask, int& A1, int& A2, int& zmin, int& zmax, int& zmed, int& zxy);
+
 public:
     void histogramEqualization(const Mat& im, Mat& finalResult, vector<double>& histogram);
     Mat imgThresholding(const Mat& im, double T = -1);
     void preProcessing(const Mat& im, Mat& resized_im, Mat& gray_im, Mat& eq_im, Mat& filtered_im, Mat& bin_im, bool show);
+    void extractPlateRegion(const Mat& eq_im, const Mat& bin_im, Mat& extracted_im, bool show);
+    void extractCharacters(const Mat& extracted_plate, const Size& charSize, Mat& characters, bool show);
 };
