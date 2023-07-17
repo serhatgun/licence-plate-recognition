348564ff0dc7fee9542048777ecbb715e2b71998
Serhat Gün <36711858+serhatgun@users.noreply.github.com>
2023-07-17 13:56:34 +0300

    Image thresholding and image preprocessing functions are added.

--- utils.h
+++ utils.h
@@ -17,4 +17,6 @@ private:
 
 public:
     void my_histogram_equalization(const Mat& im, Mat& finalResult, vector<double>& histogram);
+    Mat my_imgthresholding(const Mat& im, double T = -1);
+    void preprocessing(const Mat& im, Mat& resized_im, Mat& gray_im, Mat& eq_im, Mat& filtered_im, Mat& bin_im, bool show);
 };
